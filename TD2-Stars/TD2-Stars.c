0ef1e6acf14cd174d436a9edb94b82027c7b04fa
Julien Flusin <julienflusin3@gmail.com>
2020-12-14 16:15:05 +0100

    Question 1 exo For

--- TD2-Stars/TD2-Stars.c
+++ TD2-Stars/TD2-Stars.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+int x, y;
+int hauteur = 10;
+
 int main()
 {
-    
+    for (y = 1; y <= hauteur; y++) {
+        for (x = 1; x <= y; y++) {
+            printf("*");
+        }
+        printf("\n");
+    }
 }

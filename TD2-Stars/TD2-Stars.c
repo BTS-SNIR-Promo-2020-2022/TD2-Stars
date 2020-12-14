d6b468543bea97ed30e46d95027e13061dd06558
Julien Flusin <julienflusin3@gmail.com>
2020-12-14 16:48:19 +0100

    Question 4 Exo For

--- TD2-Stars/TD2-Stars.c
+++ TD2-Stars/TD2-Stars.c
@@ -4,10 +4,11 @@ int x, y, hauteur;
 
 int main()
 {
-    printf("Entre une hauteur !\n");
-    scanf("%d", &hauteur);
-    for (y = 1; y <= hauteur; y++) {
-        for (x = 1; x <= y; x++) {
+    //printf("Entre une hauteur !\n");
+    //scanf("%d", &hauteur);
+    hauteur = 10;
+    for(y = 0; y<hauteur; y++) {
+        for(x = 0; x<hauteur-y; x++) {
             printf("*");
         }
         printf("\n");
